66e0f3f9ea27a2ef2347cdd2133f255ddf6af4f8
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2025-10-20 19:38:01 +0600

    Created the Category Model and Migration , Add Category to Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -11,4 +12,9 @@ class Product extends Model
         'description',
         'price',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
